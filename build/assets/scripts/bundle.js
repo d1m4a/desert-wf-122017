245efb1c8315c8a4055aa6b2f9100e86911796cb
Dmitry Chekh <dmitry.peleev@gmail.com>
2017-12-27 01:12:41 +0700

    настройка preloader и кнопки menu

--- build/assets/scripts/bundle.js
+++ build/assets/scripts/bundle.js
@@ -266,7 +266,10 @@ module.exports = preloader;
 
 (function() {
 
-    var toggles = document.querySelectorAll(".menu__toggle");
+    const toggles = document.querySelectorAll(".menu__toggle");
+    const container = document.querySelector('.menu__container');
+    const menu = document.querySelector('.menu__list');
+
    
     for (var i = toggles.length - 1; i >= 0; i--) {
       var toggle = toggles[i];
@@ -276,7 +279,15 @@ module.exports = preloader;
     function toggleHandler(toggle) {
       toggle.addEventListener( "click", function(e) {
         e.preventDefault();
-        (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+        if(this.classList.contains("active") === true) { 
+          this.classList.remove("active")
+          container.classList.remove('menu__container--open')
+          menu.classList.remove('menu__list--open')
+        } else { 
+          this.classList.add("active");
+          container.classList.add('menu__container--open')
+          menu.classList.add('menu__list--open')
+        }
       });
     }
    
--- src/scripts/common/gumburger.js
+++ src/scripts/common/gumburger.js
@@ -1,6 +1,9 @@
 (function() {
 
-    var toggles = document.querySelectorAll(".menu__toggle");
+    const toggles = document.querySelectorAll(".menu__toggle");
+    const container = document.querySelector('.menu__container');
+    const menu = document.querySelector('.menu__list');
+
    
     for (var i = toggles.length - 1; i >= 0; i--) {
       var toggle = toggles[i];
@@ -10,7 +13,15 @@
     function toggleHandler(toggle) {
       toggle.addEventListener( "click", function(e) {
         e.preventDefault();
-        (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+        if(this.classList.contains("active") === true) { 
+          this.classList.remove("active")
+          container.classList.remove('menu__container--open')
+          menu.classList.remove('menu__list--open')
+        } else { 
+          this.classList.add("active");
+          container.classList.add('menu__container--open')
+          menu.classList.add('menu__list--open')
+        }
       });
     }
    
